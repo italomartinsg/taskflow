6242b029dfe763e3e3e5cb430dbfa2244910a821
Ítalo Martins <italomartinsg@gmail.com>
2026-09-14 23:26:50 -0300

    feat: add task priority

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const taskCreationForm = document.querySelector("#form-criar-task");
 const taskTitle = document.querySelector("#titulo-tarefa");
 const taskList = document.querySelector(".task-fazer");
+const taskPriority = document.querySelector("#prioridade-task");
 
 const tasks = [
-  { id: 1, title: "Estudar JavaScript" },
-  { id: 2, title: "Atualizar currículo" },
-  { id: 3, title: "Treinar" },
+  { id: 1, title: "Estudar JavaScript", priority: "high" },
+  { id: 2, title: "Atualizar currículo", priority: "medium" },
+  { id: 3, title: "Treinar", priority: "high" },
 ];
 
 function getId() {
@@ -24,6 +25,7 @@ function renderTasks() {
   tasks.forEach((task) => {
     const taskItem = document.createElement("li");
     const btnRemove = document.createElement("button");
+    const priority = document.createElement("p");
 
     taskItem.dataset.id = task.id;
     taskItem.textContent = task.title;
@@ -31,6 +33,19 @@ function renderTasks() {
     btnRemove.textContent = "Remover";
     btnRemove.dataset.action = "remove";
 
+    switch (task.priority) {
+      case "medium":
+        priority.textContent = `Prioridade: Média`;
+        break;
+      case "high":
+        priority.textContent = `Prioridade: Alta`;
+        break;
+      default:
+        priority.textContent = `Prioridade: Baixa`;
+        break;
+    }
+
+    taskItem.appendChild(priority);
     taskItem.appendChild(btnRemove);
     taskList.appendChild(taskItem);
   });
@@ -46,10 +61,12 @@ function handleCreateTask(event) {
   const taskObj = {
     id: getId(),
     title: title,
+    priority: taskPriority.value,
   };
 
   tasks.push(taskObj);
   taskTitle.value = "";
+  taskPriority.value = "low";
   renderTasks();
 }
 function removeTask(id) {
